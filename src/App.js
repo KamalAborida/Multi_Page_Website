d178a73097c78f371e5512ff7debe281a504eb61
KamalAborida <kamal.o.aborida@gmail.com>
2024-03-21 13:42:47 +0200

    predeploy 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import React from "react";
 function App() {
   const routes = createBrowserRouter([
     {
-      path: "/",
+      path: "Multi_Page_Website/",
       element: <RootPage />,
       children: [
         {
